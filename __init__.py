22c0170f7169de5de6eb4aa54fca2f2835b90a9f
Margriet Palm <m.m.palm@lacdr.leidenuniv.nl>
2018-08-09 09:33:16 +0200

    Move matplotlib dependent functions to separate file

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from cpm_tools.visualization_2d import *
+from cpm_tools.visualization_2d_mpl import *
---
+++ visualization_2d_mpl.py
@@ -0,0 +1,81 @@
+import future        # pip install future
+import matplotlib.pyplot as plt
+import numpy as np
+import copy,os
+import matplotlib.colors as mpl_colors
+import matplotlib.animation as animation
+
+
+def animate_cpm_sim_mpl(tau_list,colors,fn=None,show=False,dpi=100,scale=1,fps=5):
+    """ Animate cpm simulation using matplotlib
+
+    :param tau_list: list of grids with tau
+    :param colors: list of matplotlib colornames, in the order of tau (see https://matplotlib.org/examples/color/named_colors.html)
+    :param fn: filename used for saving the video
+    :param show: show animation
+    :param dpi: dpi
+    :param scale: scaling factor
+    :param fps: animation frame rate
+    """
+
+    # setup figure
+    fig = plt.figure(figsize=(scale * tau_list[0].shape[0] / 100., scale * tau_list[0].shape[1] / 100.),dpi=dpi)
+    ax = plt.gca()
+
+    # setup colormap
+    cmap = mpl_colors.ListedColormap(colors)
+    bounds = np.arange(0, 7)
+    norm = mpl_colors.BoundaryNorm(bounds, cmap.N)
+
+    # create images
+    ims = []
+    for tau in tau_list:
+        im = plt.imshow(tau, interpolation='nearest', origin='lower',
+                        cmap=cmap, norm=norm, animated=True)
+        ims.append([im])
+    ax.set_aspect('equal', adjustable='box')
+    plt.axis('off')
+
+    # create animation
+    anim = animation.ArtistAnimation(fig, ims, interval=1000./fps, blit=True,repeat_delay=1000)
+    if show:
+        plt.show()
+    if fn is not None:
+        anim.save(fn, fps=fps, extra_args=['-vcodec', 'libx264'])
+    return anim
+
+
+def draw_cpm_grid_mpl(tau,colors,ax=None,fn=None,dpi=100,scale=1):
+    """ Draw cpm grid using matplotlib.
+
+    Drawing the cpm grid using matplotlib is very fast, but does not support drawing
+    cell borders.
+
+    :param tau: array with cell types
+    :param colors: list of matplotlib colornames, in the order of tau (see https://matplotlib.org/examples/color/named_colors.html)
+    :param ax: matplotlib ax to use
+    :param fn: filename used for saving
+    :param dpi: output image dpi
+    :param scale: scaling factor
+    """
+
+    # create axis object
+    if ax is None:
+        fig = plt.figure(figsize=(scale*tau.shape[0]/float(dpi),scale*tau.shape[1]/float(dpi)))
+        ax = plt.gca()
+
+    # setup color map
+    cmap = mpl_colors.ListedColormap(colors)
+    bounds=np.array(list(np.unique(tau))+[np.max(tau)+1])
+    norm = mpl_colors.BoundaryNorm(bounds, cmap.N)
+
+    # draw image
+    img = plt.imshow(tau, interpolation='nearest', origin='lower',
+                    cmap=cmap, norm=norm)
+    ax.set_aspect('equal', adjustable='box')
+    plt.axis('off')
+
+    # save image
+    if fn is not None:
+        plt.savefig(fn,dpi=dpi)
+        plt.close()
